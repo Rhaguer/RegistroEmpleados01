1412ec4c54b34d1ee446576368f4dd9f411aec8e
irihm mor20 <NASHO_204x@hotmail.com>
2024-12-19 00:57:21 -0300

    Descripción de los cambios realizados

--- RegistroEmpleados.AppMovil/MauiProgram.cs
+++ RegistroEmpleados.AppMovil/MauiProgram.cs
@@ -35,8 +35,17 @@ namespace RegistroEmpleados.AppMovil
 
             if (cargos.Result.Count == 0) 
             {
-                client.Child("Cursos").PostAsync(new Curso { Nombre = "1ro Básico" });
-                client.Child("Cursos").PostAsync(new Curso { Nombre = "3ro Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "1er Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "2do Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "3ero Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "4to Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "5to Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "6to Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "7mo Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "8vo Básico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "1er Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "2do Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "3ero Medio" });
                 client.Child("Cursos").PostAsync(new Curso { Nombre = "4to Medio" });
             }
         }
